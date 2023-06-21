1865fa3f66888c12523071019afc213627714a3a
DC-ON\de.seo <de.seo@dc-on.co.kr>
2023-06-21 17:38:23 +0900

    달력, 냉장고 추가

--- src/pages/cafeteria/Refrigerator.js
+++ src/pages/cafeteria/Refrigerator.js
@@ -1,5 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "../../Component/MyButton";
+
 const Refrigerator = () => {
-  return <div className="Refrigerator">냉장고</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="Refrigerator">
+      <h2>냉장고</h2>
+      <div>냉장고엔 생수 3병과 누군가 넣어둔 삼각김밥이 보인다.</div>
+      <MyButton text={"카페테리아"} onClick={() => navigate("/cafeteria")} />
+      <MyButton text={"자리로 돌아가기"} onClick={() => navigate("/")} />
+    </div>
+  );
 };
 
 export default Refrigerator;
